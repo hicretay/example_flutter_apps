74537da2c6d7d67920cbe4c21be9961b32dc4dc3
Hicret Ay <46728765+hicrett@users.noreply.github.com>
2021-03-08 20:00:22 +0300

    08.03.2021

---
+++ checkboxlisttile/lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State {
+  List<String> checkedItems = [];
+  Map<String, bool> language = {
+    "Dart": false,
+    "Java": false,
+    "C#": false,
+    "Python": false,
+    "Delphi": false,
+    "Kotlin": false,
+    "HTML": false,
+    "CSS": false,
+    "Javascript": false,
+    "PHP": false
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+          title: Text("Programlama Dilleri"),
+        ),
+        body: Column(
+          children: [
+            SizedBox(height: 15),
+            Text(
+              "Bildiğiniz programlama dillerini seçiniz: ",
+              style: TextStyle(fontSize: 18),
+            ),
+            Divider(
+              thickness: 2,
+            ),
+            Flexible(
+              child: Card(
+                child: ListView(
+                  children: language.keys.map((String key) {
+                    return CheckboxListTile(
+                        title: Text(key),
+                        activeColor: Colors.amber,
+                        controlAffinity: ListTileControlAffinity.leading,
+                        value: language[key],
+                        onChanged: (bool value) {
+                          setState(() {
+                            language[key] = value;
+                            if (value == true) {
+                              checkedItems.add(key);
+                            } else if (value == false) {
+                              checkedItems.remove(key);
+                            }
+                          });
+                        });
+                  }).toList(),
+                ),
+              ),
+            ),
+            Flexible(
+              child: buildListView(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  buildListView() {
+    return ListView.builder(
+      itemCount: checkedItems.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Text(
+          checkedItems[index],
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 22),
+        );
+      },
+    );
+  }
+}
